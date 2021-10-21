a1a30364af94e4002b7109a4d85166807cea60c5
shiraz26 <78658564+shiraz26@users.noreply.github.com>
2021-10-21 15:11:41 +0400

    update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="author" content="Sahil Kumar">
+  <meta name="author" content="Shiraz">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Shopping Cart System</title>
